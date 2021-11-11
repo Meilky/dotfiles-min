b92a2e21ee8d2bc627853d7c0f223bf94a56dbaf
Meilky <mikaeljean2k3@gmail.com>
2021-11-11 14:20:17 -0500

    Add dep

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+wget -O ~/.oh-my-zsh/themes/palenight.zsh-theme \
+https://raw.githubusercontent.com/jenssegers/palenight.zsh-theme/master/palenight.zsh-theme
+
 # install tmux package manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
