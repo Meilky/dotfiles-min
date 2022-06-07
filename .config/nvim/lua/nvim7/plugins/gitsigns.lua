abc9dd554a292fb3f811ca2375330a082fec670a
Meilky <53416287+Meilky@users.noreply.github.com>
2022-06-07 15:31:59 -0400

    Create gitsigns.lua

---
+++ .config/nvim/lua/nvim7/plugins/gitsigns.lua
@@ -0,0 +1,6 @@
+require('gitsigns').setup({
+        signcolumn = false,  -- Toggle with `:Gitsigns toggle_signs`
+        numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl     = true, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+})
